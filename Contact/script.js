7bf523f2a7e229624bc4e935c4a9298b5b56f71a
Soroush Mohamadian <61268168+Soroush-Mohamadian@users.noreply.github.com>
2020-06-10 14:45:31 +0430

    render table added

--- Contact/script.js
+++ Contact/script.js
@@ -1,5 +1,6 @@
 function toggleForm() {
     ($('#form').css('width') === '0px') ? showForm() : hideForm();
+    
 
 }
 
@@ -16,47 +17,62 @@ function hideForm() {
     $('#form').css('padding', '0px')
 }
 
-let contacts = [
-    {
-        id: '1',
-        Name: 'Soroush',
-        Surname: 'Mohamadian',
-        Phone: '09219652265',
-        Email: 'soroushmdn@gmail.com',
-        Address: 'Tehran',
-        birthday: '78/1/27',
-        Details: 'Me'
+$('#form').on('submit', function (event) {
+    console.log('hello')
+    event.preventDefault();
 
-    }, {
-        id: '113',
-        Name: 'Sina',
-        Surname: 'Mohamadian',
-        Phone: '09219652265',
-        Email: 'sina@gmail.com',
-        Address: 'Tehran',
-        birthday: '78/1/27',
-        Details: 'Brother'
+    let contacts = JSON.parse(localStorage.getItem('contacts'))
+    const newContactData = new FormData(event.target)
+    let newContact = {}
+    for (const item of objenewContactData.entries()) {
+        newContact[item[0]]=item[1]
     }
-]
+    console.log(newContact)
+    contacts.push(newContact)
+    localStorage.setItem('contacts' , JSON.stringify(contacts))
+    hideForm()
+    renderTable()
+})
+
 function renderTable() {
-    
-    const tr = document.createElement('tr');
-    tr.innerHTML = `
-    <td>${$('#id_id_number').innerText()}</td>
-    <td>${contact.Name}</td>
-    <td>${contact.Surname}</td>
-    <td>${contact.phone}</td>
-    <td>${contact.Email}</td>
-    <td>${contact.Address}</td>
-    <td>${contact.birthday}</td>
-    <td>${contact.Details}</td>
-    <td class="buttons"> 
-    <button type="button" 
-    onclick="deletItem(${contact.id})" class="btn btn-danger"><i
-  class="fa fa-trash" aria-hidden="true"></i></button>
-<button type="button" onclick="edit(${contact.id})" class="btn btn-primary"><i
-  class="fa fa-pencil-square" aria-hidden="true"></i></button> </td>`;
-    return $('tbody').append(tr);
-  });
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (!contacts) {
+      localStorage.setItem('contacts', JSON.stringify([]));
+      return;
+    }
+    contacts.map(contact => {
+      const tr = document.createElement('tr');
+      tr.innerHTML = `
+      <td>${contact.id}</td>
+      <td>${contact.name}</td>
+      <td>${contact.surname}</td>
+      <td>${contact.phoneNumber}</td>
+      <td>${contact.email}</td>
+      <td>${contact.address}</td>
+      <td>${contact.birthday}</td>
+      <td>${contact.details}</td>
+      <td class="buttons"> 
+      <button type="button" 
+      onclick="deletItem(${contact.id})" class="btn btn-danger"><i
+    class="fa fa-trash" aria-hidden="true"></i></button>
+  <button type="button" onclick="edit(${contact.id})" class="btn btn-primary"><i
+    class="fa fa-pencil-square" aria-hidden="true"></i></button> </td>`;
+      return $('tbody').append(tr);
+    });
+  }
+
+  $('#form_contact').on('submit', function(event) {
+    event.preventDefault();
+    //
+    let contacts = JSON.parse(localStorage.getItem('contacts'));
+    const newContactData = new FormData(event.target).entries();
+    const newContact = Object.fromEntries(newContactData);
+    contacts.push(newContact);
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+    renderTable();
+    hideForm();
+    event.target.reset();
+
+  
 
 
